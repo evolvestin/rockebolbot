5d7974ecc40c7465962edd421edb7c729747131b
evolvestin <36014630+evolvestin@users.noreply.github.com>
2018-02-01 03:25:15 +0300

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,7 +1,14 @@
-import telebot
+import requests
 
-API_TOKEN = '462725941:AAFxYxX0G_smCog6ZS-f2T_vqVfdUwCTRH4'
+url = "https://api.telegram.org/bot462725941:AAFxYxX0G_smCog6ZS-f2T_vqVfdUwCTRH4/"
 
-bot = telebot.TeleBot(API_TOKEN)
 
-bot.send_message(396978030, "test")
+def get_updates_json(request):  
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):  
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
