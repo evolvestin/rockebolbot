ca265cb5a0fb21d42b955bfc2ac41fbc73b1e72a
evolvestin <36014630+evolvestin@users.noreply.github.com>
2018-02-01 03:22:30 +0300

    Update spy.py

--- spy.py
+++ spy.py
@@ -4,19 +4,4 @@ API_TOKEN = '462725941:AAFxYxX0G_smCog6ZS-f2T_vqVfdUwCTRH4'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""" + message.from_user.first_name)
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-	
-
-bot.polling()
+bot.send_message(396978030, "test")
