7767a20ad5342659fa6b4a68b4e1c78b6d8a101c
evolvestin <36014630+evolvestin@users.noreply.github.com>
2018-02-06 20:22:19 +0300

    Update spy.py

--- spy.py
+++ spy.py
@@ -20,15 +20,15 @@ def repeat_all_messages(message): # Название функции не игр�
     if message.chat.id == 491625180: #be
         bot.send_message(-1001116128920, "С в " + message.text)
     elif message.chat.id == 394850016: #ow
-        bot.send_message(-1001116128920, "Ч в " + message.text)
+        bot.send_message(-1001175146945, "Ч в " + message.text)
     elif message.chat.id == 485591553: #so
         bot.send_message(-1001116128920, "деф Ж в " + message.text)
     elif message.chat.id == 430602902: #fb
-        bot.send_message(-1001116128920, "Б в " + message.text)
+        bot.send_message(-1001175146945, "Б в " + message.text)
     elif message.chat.id == 497892874: #gt
-        bot.send_message(-1001116128920, "деф К в " + message.text)
+        bot.send_message(-1001175146945, "деф К в " + message.text)
     elif message.chat.id == 350037139: #sm
-        bot.send_message(-1001116128920, "деф Су в " + message.text)
+        bot.send_message(-1001175146945, "деф Су в " + message.text)
 
 
 if __name__ == '__main__':
