9c6e76b06eb50b634420731ad3f65464159ebede
evolvestin <36014630+evolvestin@users.noreply.github.com>
2018-02-10 04:52:01 +0300

    Update spy.py

--- spy.py
+++ spy.py
@@ -12,6 +12,8 @@
 import telebot
 import urllib3
 token = "462725941:AAFxYxX0G_smCog6ZS-f2T_vqVfdUwCTRH4"
+url = 'http://bitlux.ru/evolve.php?text=les'
+http = urllib3.PoolManager()
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
@@ -33,7 +35,7 @@ def repeat_all_messages(message): # Название функции не игр�
     elif message.chat.id == 396978030: #kke
         bot.send_message(396978030, "дебил: " + message.text)
     elif message.chat.id == 396978030:
-        content = urllib.request.urlopen('http://bitlux.ru/evolve.php?text=les').read()
+        content = http.request('GET', url)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
